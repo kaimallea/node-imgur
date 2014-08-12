b367c9ca581b9921080c053332ade17f2d8a6949
Kai Mallea <kai@spotify.com>
2014-08-12 16:42:16 -0400

    Moar
    + Update comments
    + Add a couple of tests

--- test/imgurRequest.js
+++ test/imgurRequest.js
@@ -1,31 +1,29 @@
-(function() {
-    'use strict';
+var imgur = require('../lib/imgur.js'),
+    chai = require('chai'),
+    chaiAsPromised = require('chai-as-promised'),
+    expect = chai.expect,
+    imgurTestId1 = 'mbgq7nd', // Kitten
+    imgurTestId2 = 'ysjVczY'; // Halle Berry
 
-    var imgur = require('../lib/imgur.js'),
-        chai = require('chai'),
-        chaiAsPromised = require('chai-as-promised'),
-        expect = chai.expect;
+chai.use(chaiAsPromised);
 
-    chai.use(chaiAsPromised);
-
-    describe('#_imgurRequest()', function() {
-        beforeEach(function() {
-        });
-
-        it('should fail with no input', function(done) {
-            var errMsg = 'Invalid argument';
-
-            expect(imgur._imgurRequest())
-                .to.be.rejectedWith(errMsg)
-                .and.notify(done);
-        });
-
-        it('should fail with an invalid type', function(done) {
-            var errMsg = 'Invalid operation';
-
-            expect(imgur._imgurRequest('blah', 'ysjVczY'))
-                .to.be.rejectedWith(errMsg)
-                .and.notify(done);
-        });
+describe('#_imgurRequest()', function() {
+    beforeEach(function() {
     });
-}());
+
+    it('should fail with no input', function(done) {
+        var errMsg = 'Invalid argument';
+
+        expect(imgur._imgurRequest())
+            .to.be.rejectedWith(errMsg)
+            .and.notify(done);
+    });
+
+    it('should fail with an invalid operation specified', function(done) {
+        var errMsg = 'Invalid operation';
+
+        expect(imgur._imgurRequest('blah', imgurTestId2))
+            .to.be.rejectedWith(errMsg)
+            .and.notify(done);
+    });
+});
