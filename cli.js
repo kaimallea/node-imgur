49887380ad2775f162b8ef012bc46de58e6941f8
Kai Mallea <kai@spotify.com>
2014-08-13 13:57:53 -0400

    Fix saving/loading. Also minor logic tweaks

--- cli.js
+++ cli.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 'use strict'
-
-var imgur     = require('./lib/imgur.js');
+var imgur     = require('./lib/imgur');
 var commander = require('commander');
+var Q         = require('q');
 var util      = require('util');
 
 // Used to collect args for specific options
@@ -19,107 +19,104 @@ commander
     .option('-f, --file [file ...]', 'Upload binary image files', collect, [])
     .option('-c, --client-id [id]', 'Specify a client ID to use only for the current operation')
     .option('--save [id]', 'Save client id to disk for future use')
-    .option('--clear', 'Remove the previously saved client id')
+    .option('--clear', 'Remove previously saved client id')
     .option('--show', 'Display saved client id')
     .parse(process.argv);
 
 
-if (commander.show) {
+// @TODO: There's probably a more promisey/chainy way to make this more succinct
 
-    imgur.loadClientId()
-        .then(function (clientId) {
-            console.log(clientId);
-        })
+imgur.loadClientId()
+    .then(imgur.setClientId)
+    .fin(function () {
+        if (commander.show) {
 
-} else if (commander.save && commander.save.length) {
+            console.log(imgur.getClientId());
 
-    imgur.saveClientId(commander.save)
-        .catch(function (err) {
-            console.error('Unable to save (%s)', err.message);
-        });
+        } else if (commander.clear) {
 
-} else if (commander.clear) {
-
-    imgur.clearClientId()
-        .catch(function (err) {
-            console.error('Unable to clear (%s)', err.message);
-        });
-
-} else {
-
-    if (commander.clientId && commander.clientId.length) {
-        imgur.setClientId(commander.clientId);
-    }
-
-
-    if (commander.info && commander.info.length) {
-        commander.info.forEach(function (id, index, array) {
-            imgur.getInfo(id)
-                .then(function (json) {
-                    console.log(json.data);
-                })
-                .catch(function (err) {
-                    console.error(err.message);
+            imgur.clearClientId()
+                .fail(function (err) {
+                    console.error('Unable to clear client id (%s)', err.message);
                 });
-        });
-    }
 
+        } else if (commander.save) {
 
-    if (commander.url && commander.url.length) {
-        commander.url.forEach(function(url, index, array) {
-            imgur.uploadUrl(url)
-                .then(function (json) {
-                    var output;
-                    if (commander.url.length > 1) {
-                        output = util.format('%s -> %s', url, json.data.link);
-                    } else {
-                        output = json.data.link;
-                    }
-                    console.log(output);
-                })
-                .catch(function (err) {
-                    console.error(err.message);
+            imgur.saveClientId(commander.save)
+                .fail(function (err) {
+                    console.error('Unable to save client id (%s)', err.message);
                 });
-        });
-    }
 
+        } else {
 
-    if (commander.base64 && commander.base64.length) {
-        commander.base64.forEach(function(str, index, array) {
-            imgur.uploadBase64(str)
-                .then(function (json) {
-                    var output;
-                    if (commander.base64.length > 1) {
-                        output = util.format('%s... -> %s', str.substr(0, 7), json.data.link);
-                    } else {
-                        output = json.data.link;
-                    }
-                    console.log(output);
-                })
-                .catch(function (err) {
-                    console.error(err.message);
+            if (commander.file.length || commander.args.length) {
+                var args = commander.file.concat(commander.args);
+                args.forEach(function(file, index, array) {
+                    imgur.uploadFile(file)
+                        .then(function (json) {
+                            var output;
+                            if (args.length > 1) {
+                                output = util.format('%s -> %s', file, json.data.link);
+                            } else {
+                                output = json.data.link;
+                            }
+                            console.log(output);
+                        })
+                        .catch(function (err) {
+                            console.error('%s (%s)', err.message, file);
+                        });
                 });
-        });
-    }
+            }
 
-
-    if ( (commander.file && commander.file.length) || commander.args.length ) {
-        var args = commander.file.concat(commander.args);
-        args.forEach(function(file, index, array) {
-            imgur.uploadFile(file)
-                .then(function (json) {
-                    var output;
-                    if (args.length > 1) {
-                        output = util.format('%s -> %s', file, json.data.link);
-                    } else {
-                        output = json.data.link;
-                    }
-                    console.log(output);
-                })
-                .catch(function (err) {
-                    console.error('%s: %s', file, err.message);
+            if (commander.info.length) {
+                commander.info.forEach(function (id, index, array) {
+                    imgur.getInfo(id)
+                        .then(function (json) {
+                            console.log(json.data);
+                        })
+                        .catch(function (err) {
+                            console.error('%s (%s)', err.message, url);
+                        });
                 });
-        });
-    }
+            }
 
-}
+
+            if (commander.base64.length) {
+                commander.base64.forEach(function(str, index, array) {
+                    imgur.uploadBase64(str)
+                        .then(function (json) {
+                            var output;
+                            if (commander.base64.length > 1) {
+                                output = util.format('%s... -> %s', str.substr(0, 7), json.data.link);
+                            } else {
+                                output = json.data.link;
+                            }
+                            console.log(output);
+                        })
+                        .catch(function (err) {
+                            var output = util.format('%s (%s...)', err.message, str.substr(0, 7));
+                            console.error(output);
+                        });
+                });
+            }
+
+
+            if (commander.url.length) {
+                commander.url.forEach(function(url, index, array) {
+                    imgur.uploadUrl(url)
+                        .then(function (json) {
+                            var output;
+                            if (commander.url.length > 1) {
+                                output = util.format('%s -> %s', url, json.data.link);
+                            } else {
+                                output = json.data.link;
+                            }
+                            console.log(output);
+                        })
+                        .catch(function (err) {
+                            console.error('%s (%s)', err.message, url);
+                        });
+                });
+            }
+        }
+    });
