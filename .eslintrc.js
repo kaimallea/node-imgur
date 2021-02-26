7740a19844360780d3ec77ac40288b6ccae1971d
Kai Mallea <kai@spotify.com>
2021-02-26 15:43:40 -0500

    build(deps-dev): install jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     commonjs: true,
     es2021: true,
     node: true,
-    mocha: true,
+    jest: true,
   },
   extends: ['eslint:recommended', 'prettier'],
   parserOptions: {
