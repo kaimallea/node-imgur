d311cc8119913e3e6f5282ab49c1e7c34bae3b03
Ken Eucker <keneucker@gmail.com>
2021-10-27 21:31:30 -0700

    build(webpack): splits the compilation of the library into two separate targets: node and browser

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 const path = require('path');
-const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
 
-module.exports = {
-  context: path.resolve(__dirname),
-  devtool: 'inline-source-map',
-  entry: './src/index.ts',
-  mode: 'development',
+const generalConfig = {
+  watchOptions: {
+    aggregateTimeout: 600,
+    ignored: /node_modules/,
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false,
+      cleanOnceBeforeBuildPatterns: [path.resolve(__dirname, './dist')],
+    }),
+  ],
   module: {
     rules: [
       {
@@ -15,26 +22,50 @@ module.exports = {
       },
     ],
   },
-  plugins: [new NodePolyfillPlugin()],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    globalObject: `typeof self !== 'undefined' ? self : this`,
-    library: 'imgur',
-    libraryTarget: 'umd',
-    scriptType: 'module',
-    uniqueName: 'imgur-api',
-    auxiliaryComment: {
-      root: 'Root Comment',
-      commonjs: 'CommonJS Comment',
-      commonjs2: 'CommonJS2 Comment',
-      amd: 'AMD Comment',
-    },
-  },
   resolve: {
-    extensions: ['.tsx', '.ts', '.jsx', '.js'],
-    fallback: {
-      path: require.resolve('path-browserify'),
-    },
+    extensions: ['.tsx', '.ts', '.js'],
   },
+};
+
+const nodeConfig = {
+  devtool: 'inline-source-map',
+  entry: './src/index.ts',
+  target: 'node',
+  externals: [nodeExternals()],
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'imgur.node.js',
+    globalObject: 'this',
+    libraryTarget: 'umd',
+    libraryExport: 'default',
+  },
+};
+
+const browserConfig = {
+  devtool: 'inline-source-map',
+  entry: './src/index.ts',
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'imgur.js',
+    libraryTarget: 'umd',
+    // globalObject: 'this',
+    libraryExport: 'default',
+    umdNamedDefine: true,
+    library: 'imgur',
+  },
+};
+
+module.exports = (env, argv) => {
+  if (argv.mode === 'development') {
+    generalConfig.devtool = 'cheap-module-source-map';
+  } else if (argv.mode === 'production') {
+  } else {
+    throw new Error('Specify env');
+  }
+
+  Object.assign(nodeConfig, generalConfig);
+  Object.assign(browserConfig, generalConfig);
+
+  return [nodeConfig, browserConfig];
 };
